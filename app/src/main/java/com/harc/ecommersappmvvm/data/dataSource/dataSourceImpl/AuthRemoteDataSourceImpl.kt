53f3f445b561c3599c74b63c435c643032ceea58
Hernando Cassiani <nando64444@gmail.com>
2023-11-14 13:40:29 -0700

    Update

--- app/src/main/java/com/harc/ecommersappmvvm/data/dataSource/dataSourceImpl/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/harc/ecommersappmvvm/data/dataSource/dataSourceImpl/AuthRemoteDataSourceImpl.kt
@@ -4,14 +4,38 @@ import com.harc.ecommersappmvvm.data.dataSource.dataSource.AuthRemoteDataSource
 import com.harc.ecommersappmvvm.data.service.AuthService
 import com.harc.ecommersappmvvm.domain.model.AuthResponse
 import com.harc.ecommersappmvvm.domain.model.User
+import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.Response
+import java.net.ConnectException
 
-class AuthRemoteDataSourceImpl(private val authService: AuthService): AuthRemoteDataSource {
+class AuthRemoteDataSourceImpl(private val authService: AuthService) : AuthRemoteDataSource {
 
-    override suspend fun login(email:String, password:String) = authService.login(email, password)
-    override suspend fun register(user: User): Response<AuthResponse> = authService.register(user)
+    override suspend fun login(email: String, password: String): Response<AuthResponse> {
+        return try {
+            authService.login(email, password)
+        } catch (e: ConnectException) {
+            // Excepciones
+            // Excepciones
+            Response.error(503, "El servidor no está disponible".toResponseBody(null))
+        } catch (e: Exception) {
+            // Excepciones
+            throw e
+        }
+    }
 
+
+    override suspend fun register(user: User): Response<AuthResponse> {
+        return try {
+            authService.register(user)
+        } catch (e: Exception) {
+            Response.error(503, "El servidor no está disponible".toResponseBody(null))
+        } catch (e: Exception) {
+            // Excepciones
+            throw e
+        }
+    }
 }
+
 
 
 
